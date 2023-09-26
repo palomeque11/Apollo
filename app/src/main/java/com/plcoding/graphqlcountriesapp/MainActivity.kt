3557e2c1e792bd819abd576bc42cc8869119c7e0
omarj <omarjacp@gmail.com>
2023-09-26 09:22:24 -0600

    first commit

---
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.plcoding.graphqlcountriesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.plcoding.graphqlcountriesapp.presentation.CountriesScreen
+import com.plcoding.graphqlcountriesapp.presentation.CountriesViewModel
+import com.plcoding.graphqlcountriesapp.ui.theme.GraphQlCountriesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraphQlCountriesAppTheme {
+                val viewModel = hiltViewModel<CountriesViewModel>()
+                val state by
+                viewModel.state.collectAsState()
+                CountriesScreen(state = state, onSelectCountry = viewModel::selectCountry,
+                   onDismissCountryDialog = {viewModel::dismissCountryDialog} )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/domain/CountryClient.kt
@@ -0,0 +1,10 @@
+package com.plcoding.graphqlcountriesapp.domain
+
+import com.plcoding.CountryQuery
+
+interface CountryClient {
+
+    suspend fun getCountries(): List<SimpleCountry>
+
+    suspend fun getCountry(code: String): DetailedCountry?
+}
